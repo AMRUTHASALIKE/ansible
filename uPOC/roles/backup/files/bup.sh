9cd5f56e0d537d085cecc22b1f31973881dd7c3d
amruthasalike <amrutha@gmail.com>
2018-06-29 19:07:08 +0530

    added files

---
+++ uPOC/roles/backup/files/bup.sh
@@ -0,0 +1,7 @@
+if [ $(ls ~/backup | wc -l) -ne 0 ]
+then 
+	cp ~/backup/SpringMVCSecurityXML_v1.war	~/backup/SpringMVCSecurityXML_v2.war
+	touch -r ~/backup/SpringMVCSecurityXML_v1.war	~/backup/SpringMVCSecurityXML_v2.war
+fi 
+cp ~/SpringMVCSecurityXML.war ~/backup/SpringMVCSecurityXML_v1.war
+touch -r ~/SpringMVCSecurityXML.war ~/backup/SpringMVCSecurityXML_v1.war
